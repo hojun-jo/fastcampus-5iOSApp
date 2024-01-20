59df6cdd484ca1fb49e11b7297472c24314b81e5
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-01-21 01:20:19 +0900

    feat: WarmUp 프로젝트 생성

---
+++ WarmUp/WarmUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WarmUp
+//
+//  Created by 조호준 on 1/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WarmUp/WarmUp/WarmUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarmUpApp.swift
+//  WarmUp
+//
+//  Created by 조호준 on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct WarmUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
