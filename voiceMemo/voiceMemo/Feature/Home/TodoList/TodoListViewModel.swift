6a4612d3f6ef88498057f71d6f3a236e1e2b942e
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-01-31 19:14:07 +0900

    feat: TodoListViewModel 생성

--- voiceMemo/voiceMemo/Feature/Home/TodoList/TodoListViewModel.swift
+++ voiceMemo/voiceMemo/Feature/Home/TodoList/TodoListViewModel.swift
@@ -5,6 +5,72 @@
 
 import Foundation
 
-class TodoListViewModel {
-  
+class TodoListViewModel: ObservableObject {
+    @Published var todos: [Todo]
+    @Published var isEditTodoMode: Bool
+    @Published var removeTodos: [Todo]
+    @Published var isDisplayRemoveTodoAlert: Bool
+    
+    var removeTodoCount: Int {
+        return removeTodos.count
+    }
+    var navigationBarRightButtonMode: NavigationBtnType {
+        isEditTodoMode ? .complete : .edit
+    }
+    
+    init(
+        todos: [Todo] = [],
+        isEditModeTodoMode: Bool = false,
+        removeTodos: [Todo] = [],
+        isDisplayRemoveTodoAlert: Bool = false
+    ) {
+        self.todos = todos
+        self.isEditTodoMode = isEditModeTodoMode
+        self.removeTodos = removeTodos
+        self.isDisplayRemoveTodoAlert = isDisplayRemoveTodoAlert
+    }
+}
+
+extension TodoListViewModel {
+    func selectedBoxTapped(_ todo: Todo) {
+        if let index = todos.firstIndex(where: { $0 == todo }) {
+            todos[index].selected.toggle()
+        }
+    }
+    
+    func addTodo(_ todo: Todo) {
+        todos.append(todo)
+    }
+    
+    func navigationRightButtonTapped() {
+        if isEditTodoMode {
+            if removeTodos.isEmpty {
+                isEditTodoMode = false
+            } else {
+                setIsDisplayRemoveTodoAlert(true)
+            }
+        } else {
+            isEditTodoMode = true
+        }
+    }
+    
+    func setIsDisplayRemoveTodoAlert(_ isDisplay: Bool) {
+        isDisplayRemoveTodoAlert = isDisplay
+    }
+    
+    func todoRemoveSelectedBoxTapped(_ todo: Todo) {
+        if let index = removeTodos.firstIndex(of: todo) {
+            removeTodos.remove(at: index)
+        } else {
+            removeTodos.append(todo)
+        }
+    }
+    
+    func removeButtonTapped() {
+        todos.removeAll { todo in
+            removeTodos.contains(todo)
+        }
+        removeTodos.removeAll()
+        isEditTodoMode = false
+    }
 }
