4ee326daf04409969fbb495af2fe2d91b2d91816
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-01-31 20:02:55 +0900

    feat: TodoListView의 TitleView 생성

--- voiceMemo/voiceMemo/Feature/Home/TodoList/ToodoListView.swift
+++ voiceMemo/voiceMemo/Feature/Home/TodoList/ToodoListView.swift
@@ -25,11 +25,31 @@ struct TodoListView: View {
                     Spacer()
                         .frame(height: 30)
                 }
+                
+                TitleView()
             }
         }
     }
 }
 
+// MARK: - TodoList 타이틀 뷰
+private struct TitleView: View {
+    @EnvironmentObject private var todoListViewModel: TodoListViewModel
+    
+    fileprivate var body: some View {
+        HStack {
+            if todoListViewModel.todos.isEmpty {
+                Text("To do list를\n추가해 보세요.")
+            } else {
+                Text("To do list \(todoListViewModel.todos.count)개가\n있습니다.")
+            }
+            
+            Spacer()
+        }
+        .font(.system(size: 30, weight: .bold))
+        .padding(.leading, 20)
+    }
+}
 struct TodoListView_Previews: PreviewProvider {
     static var previews: some View {
         TodoListView()
