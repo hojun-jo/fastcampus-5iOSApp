dc0185a11a65cab35e59dde1f52c101b83853fca
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-01-31 21:56:53 +0900

    feat: TodoView의 TitleView 생성

--- voiceMemo/voiceMemo/Feature/Home/TodoList/Todo/TodoView.swift
+++ voiceMemo/voiceMemo/Feature/Home/TodoList/Todo/TodoView.swift
@@ -9,6 +9,18 @@ struct TodoView: View {
   var body: some View {
     Text("Todo")
   }
+// MARK: - 타이틀 뷰
+private struct TitleView: View {
+    fileprivate var body: some View {
+        HStack {
+            Text("To do list를\n추가해 보세요.")
+            
+            Spacer()
+        }
+        .font(.system(size: 30, weight: .bold))
+        .padding(.leading, 20)
+    }
+}
 }
 
 struct TodoView_Previews: PreviewProvider {
