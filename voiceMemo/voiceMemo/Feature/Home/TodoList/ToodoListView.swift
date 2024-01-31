656dba4b05a7ed9cbab8bf5f95d1f23cfe89479f
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-01-31 20:01:58 +0900

    feat: TodoListView 생성

--- voiceMemo/voiceMemo/Feature/Home/TodoList/ToodoListView.swift
+++ voiceMemo/voiceMemo/Feature/Home/TodoList/ToodoListView.swift
@@ -6,13 +6,34 @@
 import SwiftUI
 
 struct TodoListView: View {
-  var body: some View {
-    Text("Todo List")
-  }
+    @EnvironmentObject private var pathModel: PathModel
+    @EnvironmentObject private var todoListViewModel: TodoListViewModel
+    
+    var body: some View {
+        ZStack {
+            // todo cell list
+            VStack {
+                if !todoListViewModel.todos.isEmpty {
+                    CustomNavigationBar(
+                        isDisplayLeftButton: false,
+                        rightButtonAction: {
+                            todoListViewModel.navigationRightButtonTapped()
+                        },
+                        rightButtonType: todoListViewModel.navigationBarRightButtonMode
+                    )
+                } else {
+                    Spacer()
+                        .frame(height: 30)
+                }
+            }
+        }
+    }
 }
 
 struct TodoListView_Previews: PreviewProvider {
-  static var previews: some View {
-    TodoListView()
-  }
+    static var previews: some View {
+        TodoListView()
+            .environmentObject(PathModel())
+            .environmentObject(TodoListViewModel())
+    }
 }
